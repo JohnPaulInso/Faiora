75862ca4eac795f94fa45f312602165126f9bc8b
JohnPaulInso <88543202+JohnPaulInso@users.noreply.github.com>
2026-04-14 01:03:58 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.faiora.app',
+  appName: 'faiora',
+  webDir: 'www'
+};
+
+export default config;
