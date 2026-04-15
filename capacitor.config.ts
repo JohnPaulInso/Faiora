b3dc14d7b1dde815775d4838534c96596c7b7acc
JohnPaulInso <88543202+JohnPaulInso@users.noreply.github.com>
2026-04-16 00:24:55 +0800

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.faiora.app',
-  appName: 'faiora',
-  webDir: 'www'
+  appName: 'Faiora',
+  webDir: 'www',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: true,
+      providers: ['google.com']
+    }
+  }
 };
 
 export default config;
