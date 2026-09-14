1ac1c7e2cde2c401c536debb67d8c1deb077614d
John Paul Inso <john.paulinso@fusionos.ai>
2026-09-14 21:00:18 +0800

    Update: 2026-09-14 21:00

--- android/app/src/main/java/com/faiora/app/MainActivity.java
+++ android/app/src/main/java/com/faiora/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.faiora.app;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,13 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // (2026-07-13) Lock portrait for phones, allow on tablet. Prev: unspecified
+        if (getResources().getConfiguration().smallestScreenWidthDp < 600) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
 
         // (2026-07-13) Set transparent status bar & edge-to-edge layout. Prev: default
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
